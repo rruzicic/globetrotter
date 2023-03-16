7505e9c0abe73bf240dae61ffa9c7cd2cc52a799
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-03-16 13:03:19 +0100

    Add api key struct to user struct

--- flights/backend/models/user.go
+++ flights/backend/models/user.go
@@ -2,10 +2,10 @@ package models
 
 type User struct {
 	Model
-	FirstName string `json:"firstName" bson:"first_name" binding:"required"`
-	LastName  string `json:"lastName" bson:"last_name" binding:"required"`
-	EMail     string `json:"email" bson:"email" binding:"required,email"`
-	Password  string `json:"password" bson:"password"`
-	APIKey    string `json:"apiKey" bson:"api_key"`
+	FirstName string  `json:"firstName" bson:"first_name" binding:"required"`
+	LastName  string  `json:"lastName" bson:"last_name" binding:"required"`
+	EMail     string  `json:"email" bson:"email" binding:"required,email"`
+	Password  string  `json:"password" bson:"password"`
+	APIKey    API_Key `json:"apiKey" bson:"api_key"`
 	Address
 }
