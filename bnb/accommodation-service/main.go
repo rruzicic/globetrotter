14b27324a31a6f9f999254abe7c239e432d35168
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-05-09 20:21:49 +0200

    Add gin setup

--- bnb/accommodation-service/main.go
+++ bnb/accommodation-service/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/rruzicic/globetrotter/bnb/accommodation-service/repos"
 )
 
 func main() {
 	repos.Connect()
 	repos.Disconnect()
+}
+
+func ginSetup() {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+	r.NoRoute()
+
+	r.Group("/accommodation")
+
+	r.Run(":8081")
 }
