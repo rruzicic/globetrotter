99eb32d2ab6d3619ca61bc064dc06718d2692333
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-05-09 14:52:22 +0200

    Move user connection out of main

--- bnb/accommodation-service/main.go
+++ bnb/accommodation-service/main.go
@@ -1,26 +1,13 @@
 package main
 
 import (
-	"context"
 	"log"
-	"time"
 
 	"github.com/rruzicic/globetrotter/bnb/accommodation-service/pb"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	if err != nil {
-		log.Fatalf("did not connect: %v", err)
-	}
-
-	defer conn.Close()
-	c := pb.NewUserServiceClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
+	c, ctx := makeUserServiceConnection()
 	r, err := c.GetUserById(ctx, &pb.UserRequestId{Id: "asdas"})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
