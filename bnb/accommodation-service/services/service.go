82393c76bcfa5fc05e46993aa8094dd5630dad96
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-05-13 10:30:50 +0200

    Fix docker shit

--- bnb/accommodation-service/services/service.go
+++ bnb/accommodation-service/services/service.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"log"
+
 	"github.com/rruzicic/globetrotter/bnb/accommodation-service/models"
 	"github.com/rruzicic/globetrotter/bnb/accommodation-service/repos"
 )
@@ -18,4 +20,46 @@ func UpdateAccommodation(accommodation models.Accommodation) error {
 		return err
 	}
 	return nil
+}
+
+func UpdateAvailabilityInterval(accommodation_id string, new_price float32, new_interval models.TimeInterval) (bool, error) {
+	accommodation, err := repos.GetAccommodationById(accommodation_id)
+	if err != nil {
+		log.Panic("Could not get accommodation with id: ", accommodation_id)
+		return false, err
+	}
+
+	price := models.Price{Amount: new_price, Duration: new_interval}
+
+	// connect to reservations grpc server
+	// for _, res_id := range accommodation.Reservations
+	// reservation = reservationsClient.getReservationById
+	// if price.interval.otherintervaloverlaps return false, nil
+	// disconnect from grpc server
+	// accommodation.unitprice = price
+	// err := updateaccommodation(accommodation); if err != nil
+	// log.panic(nesto)
+	// return false, err
+
+	return true, nil
+}
+
+func UpdatePriceInterval(accommodation_id string, new_interval models.TimeInterval) (bool, error) {
+	accommodation, err := repos.GetAccommodationById(accommodation_id)
+	if err != nil {
+		log.Panic("Could not get accommodation with id: ", accommodation_id)
+		return false, err
+	}
+
+	// connect to reservations grpc server
+	// for _, res_id := range accommodation.Reservations
+	// reservation = reservationsClient.getReservationById
+	// if new_interval.otherintervaloverlaps return false, nil
+	// disconnect from grpc server
+	// accommodation.availability = new_interval
+	// err := updateaccommodation(accommodation); if err != nil
+	// log.panic(nesto)
+	// return false, err
+
+	return true, nil
 }
