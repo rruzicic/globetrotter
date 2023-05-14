ed91cee4b938104b671a235625a66ab6d3c5848c
Nikola Grbovic <grbovicn.ra@gmail.com>
2023-05-14 10:02:04 +0200

    remove console log

--- bnb/frontend/bnb-frontend/src/pages/AccountPage.jsx
+++ bnb/frontend/bnb-frontend/src/pages/AccountPage.jsx
@@ -16,6 +16,7 @@ const AccountPage = () => {
                 return
             })
             .then((response) => {
+                console.log(response);
                 setUserInfo(response.data)
             })
     }, [])
