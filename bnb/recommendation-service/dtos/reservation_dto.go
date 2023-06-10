7517f45f9e96e14a02e96809a4acd82616bbeaec
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-06-10 16:40:02 +0200

    Lord forgive me for i have sinned (search flights)

--- bnb/recommendation-service/dtos/reservation_dto.go
+++ bnb/recommendation-service/dtos/reservation_dto.go
@@ -7,4 +7,5 @@ type ReservationDTO struct {
 	ReservationEndDate   time.Time `json:"reservationEndDate"`
 	ArrivalDestination   string    `json:"arrivalDestination"`   // place where you want to land when coming to the reservation place
 	DepartureDestination string    `json:"departureDestination"` // place where you want to land coming from the reservation place
+	People               int       `json:"people"`               // how many people are staying at the reservation to know minimum fligh spaces needed
 }
---
+++ bnb/recommendation-service/services/flight_service.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/rruzicic/globetrotter/bnb/recommendation-service/dtos"
+)
+
+func SearchFlights(reservationDTO dtos.ReservationDTO) ([]dtos.Flight, error) {
+	arrival_query := fmt.Sprintf("destination=%s&departure=%s&passengerNumber=%d&departureDateTime=%s&arrivalDateTime=%s", reservationDTO.ArrivalDestination, "", reservationDTO.People, reservationDTO.ReservationStartDate.String(), "")
+	departure_query := fmt.Sprintf("destination=%s&departure=%s&passengerNumber=%d&departureDateTime=%s&arrivalDateTime=%s", reservationDTO.DepartureDestination, "", reservationDTO.People, reservationDTO.ReservationStartDate.String(), "")
+	var arrival_flights []dtos.Flight
+	var departure_flights []dtos.Flight
+
+	arrival_flights_res, err := http.Get(fmt.Sprintf("flighs_service/flights/search?%s", arrival_query))
+	if err != nil {
+		return nil, err
+	}
+	defer arrival_flights_res.Body.Close()
+	arrival_flights_res_body, _ := ioutil.ReadAll(arrival_flights_res.Body)
+	if err := json.Unmarshal(arrival_flights_res_body, &arrival_flights); err != nil {
+		return nil, err
+	}
+
+	departure_flights_res, err := http.Get(fmt.Sprintf("flights_service/flgihts/search?%s", departure_query))
+	if err != nil {
+		return nil, err
+	}
+	defer departure_flights_res.Body.Close()
+	departure_flights_res_body, _ := ioutil.ReadAll(departure_flights_res.Body)
+	if err := json.Unmarshal(departure_flights_res_body, &departure_flights); err != nil {
+		return nil, err
+	}
+
+	return append(arrival_flights, departure_flights...), nil
+}
