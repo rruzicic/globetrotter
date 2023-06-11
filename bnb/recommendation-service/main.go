d59cba2f7a429bfb37dfa4183473d0c61d1fcb70
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-06-11 14:49:07 +0200

    Make driver connection to neo4j

--- bnb/recommendation-service/main.go
+++ bnb/recommendation-service/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rruzicic/globetrotter/bnb/account-service/middlewares"
 	"github.com/rruzicic/globetrotter/bnb/recommendation-service/controllers"
+	"github.com/rruzicic/globetrotter/bnb/recommendation-service/repos"
 )
 
 func main() {
-	go ginSetup()
+	ginSetup()
+	repos.Connect()
+	repos.Disconnect()
 }
 
 func ginSetup() {
+	log.Println("Setting up gin")
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
--- bnb/recommendation-service/repos/setup.go
+++ bnb/recommendation-service/repos/setup.go
@@ -1,1 +1,24 @@
 package repos
+
+import (
+	"log"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+)
+
+var neo4jDriver neo4j.Driver
+
+func Connect() {
+	driver, err := neo4j.NewDriver("bolt://neo4j:7687", neo4j.BasicAuth("neo4j", "Auth1234", ""))
+	if err != nil {
+		log.Panic("Could not connect to neo4j. Error: ", err.Error())
+	}
+
+	neo4jDriver = driver
+}
+
+func Disconnect() {
+	if err := neo4jDriver.Close(); err != nil {
+		log.Panic("Could not close driver for neo4j. Error", err.Error())
+	}
+}
