351cea6264253bb768e70cbc5660990b18bf1253
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-06-12 18:35:48 +0200

    Add base drop and mock loading for debugging

--- bnb/recommendation-service/repos/db_manipulation.go
+++ bnb/recommendation-service/repos/db_manipulation.go
@@ -1,5 +1,49 @@
 package repos
 
+import (
+	"strconv"
+	"time"
+
+	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
+	"github.com/rruzicic/globetrotter/bnb/recommendation-service/models"
+)
+
 func InitDBData() {}
 
-func DropDB() {}
+func DropDB() error {
+	session := neo4jDriver.NewSession(neo4j.SessionConfig{AccessMode: neo4j.AccessModeWrite})
+	defer session.Close()
+
+	cypher_query := "MATCH (n) DETACH DELETE n"
+
+	if _, err := session.
+		WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+			return tx.Run(cypher_query, map[string]interface{}{})
+		}); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func LoadMockData() {
+	for i := 0; i < 10; i++ {
+		curr_str := strconv.Itoa(i)
+		prev_str := strconv.Itoa(9 - i)
+		nxt_str := strconv.Itoa((9 + i) % 10)
+
+		CreateAccommodationNode(models.Accommodation{Name: "acc" + curr_str, Location: "loc" + curr_str, Price: float32(100 + i%5), MongoId: curr_str})
+		CreateUserNode(models.User{Name: "user" + curr_str, MongoId: curr_str})
+
+		CreateReviewRelationship(models.Review{Value: 2 + i%4, MongoId: curr_str, UserMongoId: curr_str, AccommodationMongoId: curr_str})
+		CreateReviewRelationship(models.Review{Value: 2 + i%4, MongoId: "10" + curr_str, UserMongoId: curr_str, AccommodationMongoId: prev_str})
+		CreateReviewRelationship(models.Review{Value: 2 + i%4, MongoId: "20" + curr_str, UserMongoId: curr_str, AccommodationMongoId: nxt_str})
+
+		CreateReservationRelationship(models.Reservation{MongoId: curr_str, UserMongoId: curr_str, AccommodationMongoId: curr_str, ReservationEnd: time.Now()})
+		CreateReservationRelationship(models.Reservation{MongoId: "10" + curr_str, UserMongoId: curr_str, AccommodationMongoId: prev_str, ReservationEnd: time.Now()})
+		CreateReservationRelationship(models.Reservation{MongoId: "20" + curr_str, UserMongoId: curr_str, AccommodationMongoId: nxt_str, ReservationEnd: time.Now()})
+	}
+	for _, id := range []int{1, 3, 5, 100, 103, 105, 200, 203, 205} {
+		UpdateReservationRelationship(models.Reservation{MongoId: strconv.Itoa(id), UserMongoId: "", AccommodationMongoId: "", ReservationEnd: time.Now().AddDate(0, -4, 0)})
+	}
+}
