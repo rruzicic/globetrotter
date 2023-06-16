7436b3fd2357f49bb5520fcceb13335227d3b2b6
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-06-16 21:15:26 +0200

    Add buy ticket for friend using api key

--- flights/backend/services/api_key_service.go
+++ flights/backend/services/api_key_service.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"log"
 	"time"
 
 	"github.com/rruzicic/globetrotter/flights/backend/models"
@@ -15,4 +16,19 @@ func AddAPIKeyToUser(user models.User, key models.APIKey) bool {
 	// Overwrites the last key!
 	user.ApiKey = key
 	return repos.UpdateUser(user)
+}
+
+func BuyTicketForFriend(flightId string, key string, numOfTicketsOptional ...int) error {
+	friend, err := repos.FindUserByAPIKey(models.APIKey{Key: key, Expiration: time.Now()})
+	if err != nil {
+		log.Print("Could not find friend with given key. Error: ", err)
+		return err
+	}
+
+	if APIKeyExpired(friend.ApiKey) {
+		log.Print("Friend's key has expired. Error: ", err)
+		return err
+	}
+
+	return BuyTicketForFriend(flightId, friend.EMail, numOfTicketsOptional...)
 }
