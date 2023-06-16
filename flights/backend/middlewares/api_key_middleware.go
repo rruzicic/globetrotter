71338428c406ea0331e67d23e4d7fe177d162d1d
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-06-16 21:10:53 +0200

    Add api key middleware

---
+++ flights/backend/middlewares/api_key_middleware.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rruzicic/globetrotter/flights/backend/models"
+	"github.com/rruzicic/globetrotter/flights/backend/pkg/http"
+	"github.com/rruzicic/globetrotter/flights/backend/repos"
+	"github.com/rruzicic/globetrotter/flights/backend/services"
+)
+
+func extractKey(ctx *gin.Context) string {
+	return ctx.Request.Header.Get("x-api-key")
+}
+
+func APIKeyMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		httpGin := http.Gin{Context: ctx}
+		key := extractKey(ctx)
+
+		user, err := repos.FindUserByAPIKey(models.APIKey{Key: key, Expiration: time.Now()})
+		if err != nil {
+			ctx.Abort()
+			httpGin.Unauthorized("No user with this key")
+			return
+		}
+
+		if services.APIKeyExpired(user.ApiKey) {
+			ctx.Abort()
+			httpGin.Unauthorized("Key expired")
+			return
+		}
+
+		ctx.Next()
+	}
+}
