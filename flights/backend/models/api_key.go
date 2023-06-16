dc18b81230d4038e67b6c49e7af60cc5602834b6
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-06-16 20:48:28 +0200

    Add creation of the api key with expiration either in 3 months or never

--- flights/backend/models/api_key.go
+++ flights/backend/models/api_key.go
@@ -3,6 +3,6 @@ package models
 import "time"
 
 type APIKey struct {
-	Key      string    `bson:"key" json:"key"`
-	Duration time.Time `bson:"duration" json:"duration"`
+	Key        string    `bson:"key" json:"key"`
+	Expiration time.Time `bson:"duration" json:"duration"`
 }
--- flights/backend/repos/api_key_repo.go
+++ flights/backend/repos/api_key_repo.go
@@ -3,6 +3,8 @@ package repos
 import (
 	"math/rand"
 	"time"
+
+	"github.com/rruzicic/globetrotter/flights/backend/models"
 )
 
 func generateAPIKeyString() string {
@@ -17,4 +19,14 @@ func generateAPIKeyString() string {
 	}
 
 	return string(byte_key)
+}
+
+func GenerateAPIKey(temporary bool) models.APIKey {
+	key := generateAPIKeyString()
+	if temporary {
+		return models.APIKey{Key: key, Expiration: time.Now().AddDate(0, 3, 0)}
+	}
+
+	// some magic to get the max time so that expiration is at the heat death of the universe
+	return models.APIKey{Key: key, Expiration: time.Unix(-2208988800, 0).Add(1<<63 - 1)}
 }
